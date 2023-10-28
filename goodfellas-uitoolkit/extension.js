f5abfeefa37682727e7a8416e662fab88ca702b6
SilverShade <aneeshyr2@gmail.com>
2023-10-28 17:28:37 +0530

    Test clipboard and extension-webview communication

--- goodfellas-uitoolkit/extension.js
+++ goodfellas-uitoolkit/extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,38 +11,47 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "goodfellas-uitoolkit" is now active!');
+	function getWebViewContent() {
+		const htmlPath = path.join(context.extensionPath, 'extensionPage', 'index.html'); // Assuming the HTML file is in the root of your extension directory
+		const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+		return htmlContent;
+	}
 	
-	let disposable = vscode.commands.registerCommand('goodfellas-uitoolkit.helloWorld', function () {
+	console.log('Congratulations, your extension "goodfellas-uitoolkit" is now active!');
+	vscode.commands.registerCommand('goodfellas-uitoolkit.helloWorld', function () {
+		vscode.window.showInformationMessage('Hello World from UIToolkit!');
+	});
+	let disposable = vscode.commands.registerCommand('goodfellas-uitoolkit.openuitoolkit', function () {
 		const panel = vscode.window.createWebviewPanel(
 			'goodfellas-uitoolkit',
-			'Hello World',
+			'UIToolkit',
 			vscode.ViewColumn.Beside,
 			{
 				enableScripts: true, // Enable JavaScript in the webview
 			}
 		);
 	
-		const htmlContent = `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>Hello World</title>
-		</head>
-		<body>
-			<h1>Hello World!</h1>
-		</body>
-		</html>`;
+		const htmlContent = getWebViewContent();
 	
 		panel.webview.html = htmlContent;
+		panel.webview.onDidReceiveMessage(message => {
+			if (message.command === 'copyButtonClicked') {
+				// Call your function here
+				// vscode.window.showInformationMessage('Copy button clicked');
+				vscode.env.clipboard.writeText("Text copied to clipboard successfully").then(() => {
+					vscode.window.showInformationMessage('Text copied to clipboard!');
+				});
+			}
+		});
 		panel.reveal();
-		vscode.window.showInformationMessage('Hello World from UIToolkit!');
 	});
 
 	
 
 	context.subscriptions.push(disposable);
 }
+
+
 
 // This method is called when your extension is deactivated
 function deactivate() {}
